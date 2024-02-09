d6bb71655de93308ea7f18682881b31ad94b41da
IsaacGaber <159034893+IsaacGaber@users.noreply.github.com>
2024-02-08 20:54:14 -0500

    added shader

---
+++ projects/webgl/test.glsl
@@ -0,0 +1,86 @@
+float sdSphere( vec3 p, float s )
+{
+  return length(p)-s;
+}
+
+float map( in vec3 pos )
+{
+    float an = 2.5*(0.5+0.5*sin(iTime*1.1+3.0));
+    vec2 c = vec2(sin(an),cos(an));
+    return sdSphere(pos, 0.3);
+}
+
+// https://iquilezles.org/articles/normalsSDF
+vec3 calcNormal( in vec3 pos )
+{
+    vec2 e = vec2(1.0,-1.0)*0.5773;
+    const float eps = 0.0005;
+    return normalize( e.xyy*map( pos + e.xyy*eps ) + 
+					  e.yyx*map( pos + e.yyx*eps ) + 
+					  e.yxy*map( pos + e.yxy*eps ) + 
+					  e.xxx*map( pos + e.xxx*eps ) );
+}
+    
+#define AA 0
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+     // camera movement	
+	float an = 0.7*iTime;
+	vec3 ro = vec3( 1.0*cos(an), 0.2, 1.0*sin(an) );
+    vec3 ta = vec3( 0.0, 0.0, 0.0 );
+    // camera matrix
+    vec3 ww = normalize( ta - ro );
+    vec3 uu = normalize( cross(ww,vec3(0.0,1.0,0.0) ) );
+    vec3 vv =          ( cross(uu,ww));
+    
+    // render
+    vec3 tot = vec3(0.0);
+    
+    #if AA>1
+    for( int m=0; m<AA; m++ )
+    for( int n=0; n<AA; n++ )
+    {
+        // pixel coordinates
+        vec2 o = vec2(float(m),float(n)) / float(AA) - 0.5;
+        vec2 p = (-iResolution.xy + 2.0*(fragCoord+o))/iResolution.y;
+        #else    
+        vec2 p = (-iResolution.xy + 2.0*fragCoord)/iResolution.y;
+        #endif
+
+	    // create view ray
+        vec3 rd = normalize( p.x*uu + p.y*vv + 1.5*ww );
+
+        // raymarch
+        const float tmax = 5.0;
+        float t = 0.0;
+        for( int i=0; i<256; i++ )
+        {
+            vec3 pos = ro + t*rd;
+            float h = map(pos);
+            if( h<0.0001 || t>tmax ) break;
+            t += h;
+        }
+        
+    
+        // shading/lighting	
+        vec3 col = vec3(0.0);
+        if( t<tmax )
+        {
+            vec3 pos = ro + t*rd;
+            vec3 nor = calcNormal(pos);
+            float dif = clamp( dot(nor,vec3(0.57703)), 0.0, 1.0 );
+            float amb = 0.5 + 0.5*dot(nor,vec3(0.0,1.0,0.0));
+            col = vec3(0.2,0.3,0.4)*amb + vec3(0.8,0.7,0.5)*dif;
+        }
+
+        // gamma        
+        col = sqrt( col * vec3(0.0, 1.0, 1.0)) ;
+	    tot += col;
+    #if AA>1
+    }
+    tot /= float(AA*AA);
+    #endif
+
+	fragColor = vec4( tot, 1.0 );
+}
