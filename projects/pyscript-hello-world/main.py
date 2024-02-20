f948017ad0f12f36e4fad4d6c5b52d729709d66f
IsaacGaber <isaacjgaber@gmail.com>
2024-02-20 11:46:57 -0500

    test filesytem

--- projects/pyscript-hello-world/main.py
+++ projects/pyscript-hello-world/main.py
@@ -1,6 +1,18 @@
-from pyscript import display
-import nltk
+import pyscript
+# import nltk
+# print(nltk)
+# display("Hello, World!", target="output", append=False)
+# display(nltk.__file__, target="output", append=False)
+# display("AAHH", target="output", append=False)
+import os
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+list_files()
 
-# def hello():
-#     display("Hello, World!", target="output", append=False)
-display(nltk.__file__, target="output", append=False)
+pyscript.display(pyscript.__file__, target="output", append=False)
