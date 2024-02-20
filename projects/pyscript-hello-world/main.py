a55f32d2a7ec90b06f39b8b7b7062a6095de8d45
IsaacGaber <isaacjgaber@gmail.com>
2024-02-20 12:47:53 -0500

    listdir as display

--- projects/pyscript-hello-world/main.py
+++ projects/pyscript-hello-world/main.py
@@ -10,15 +10,7 @@ import os
 # nltk = await fetch("https://isaacgaber.github.io/projects/pyscript-hello-world/nltk")
 #
 # print(nltk)
-
-# def list_files(startpath):
-#     for root, dirs, files in os.walk(startpath):
-#         level = root.replace(startpath, '').count(os.sep)
-#         indent = ' ' * 4 * (level)
-#         print('{}{}/'.format(indent, os.path.basename(root)))
-#         subindent = ' ' * 4 * (level + 1)
-#         for f in files:
-#             print('{}{}'.format(subindent, f))
-# list_files("../")
-
-pyscript.display(os.listdir(), target="output", append=False)
+files = ""
+for f in os.listdir():
+    files += f + "\n"
+pyscript.display(files, target="output", append=False)
