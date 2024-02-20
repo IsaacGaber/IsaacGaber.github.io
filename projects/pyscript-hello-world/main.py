c0249c9252b856b7e94594dac6bc1e79343202e4
IsaacGaber <isaacjgaber@gmail.com>
2024-02-20 12:21:58 -0500

    attempt to access nltk through URL

--- projects/pyscript-hello-world/main.py
+++ projects/pyscript-hello-world/main.py
@@ -5,14 +5,17 @@ import pyscript
 # display(nltk.__file__, target="output", append=False)
 # display("AAHH", target="output", append=False)
 import os
-def list_files(startpath):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print('{}{}/'.format(indent, os.path.basename(root)))
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print('{}{}'.format(subindent, f))
-list_files("../")
+from js import fetch
+nltk = await fetch("https://isaacgaber.github.io/projects/pyscript-hello-world/nltk")
+print(nltk)
+# def list_files(startpath):
+#     for root, dirs, files in os.walk(startpath):
+#         level = root.replace(startpath, '').count(os.sep)
+#         indent = ' ' * 4 * (level)
+#         print('{}{}/'.format(indent, os.path.basename(root)))
+#         subindent = ' ' * 4 * (level + 1)
+#         for f in files:
+#             print('{}{}'.format(subindent, f))
+# list_files("../")
 
 pyscript.display(pyscript.__file__, target="output", append=False)
