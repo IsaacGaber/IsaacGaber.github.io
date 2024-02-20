1634f3431d634a08d4c63d0b8732a6605828b475
IsaacGaber <isaacjgaber@gmail.com>
2024-02-20 09:54:57 -0500

    attempted to fix nltk import

--- projects/pyscript-hello-world/main.py
+++ projects/pyscript-hello-world/main.py
@@ -1,5 +1,6 @@
 from pyscript import display
+import nltk
 
-def hello():
-    display("Hello, World!", target="output", append=False)
-hello()
+# def hello():
+#     display("Hello, World!", target="output", append=False)
+display(nltk.__file__, target="output", append=False)
