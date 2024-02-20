8ad6588462167a0bb4a8b05e09428ca853982c1f
IsaacGaber <isaacjgaber@gmail.com>
2024-02-20 12:26:45 -0500

    print file structure

--- projects/pyscript-hello-world/main.py
+++ projects/pyscript-hello-world/main.py
@@ -5,9 +5,15 @@ import pyscript
 # display(nltk.__file__, target="output", append=False)
 # display("AAHH", target="output", append=False)
 import os
+print("Printing Structure: ")
+for f in os.walk(""):
+    print(f)
+print("Printing NLTK: ")
 from js import fetch
 nltk = await fetch("https://isaacgaber.github.io/projects/pyscript-hello-world/nltk")
+
 print(nltk)
+
 # def list_files(startpath):
 #     for root, dirs, files in os.walk(startpath):
 #         level = root.replace(startpath, '').count(os.sep)
