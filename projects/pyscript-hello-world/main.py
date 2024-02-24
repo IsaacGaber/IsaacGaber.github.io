e9c6f894b9aab06403ded29c0c61297840af332c
IsaacGaber <isaacjgaber@gmail.com>
2024-02-24 09:45:10 -0500

    added jane-brain manuial

--- projects/pyscript-hello-world/main.py
+++ projects/pyscript-hello-world/main.py
@@ -1,17 +1,8 @@
 import pyscript
-# import nltk
-# print(nltk)
-# display("Hello, World!", target="output", append=False)
-# display(nltk.__file__, target="output", append=False)
-# display("AAHH", target="output", append=False)
-import os
+import mip
 
-# from js import fetch
-# nltk = await fetch("https://isaacgaber.github.io/projects/pyscript-hello-world/nltk")
-# files = ""
-# for f in os.listdir("../mip.py"):
-#     files += f + "\n"
- # python3 -m http.server 8080 --bin --d 127.0.0.1
+mip.install("http://localhost:8080/nltk.whl", "nltk")
+from nltk import *
 test = open("test.txt", "r")
 lines = test.readlines()
 test.close()
