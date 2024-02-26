66a682d9d31352bd1b36c0682067aa05c35db63f
IsaacGaber <isaacjgaber@gmail.com>
2024-02-26 11:08:47 -0500

    added absolute links to favicons, added apple favicons

--- projects/pyscript-hello-world/main.py
+++ projects/pyscript-hello-world/main.py
@@ -1,11 +1,14 @@
-import pyscript
+from pyscript import document, display
 import mip
 
-mip.install("http://localhost:8080/nltk.whl", "nltk")
-from nltk import *
-test = open("test.txt", "r")
-lines = test.readlines()
-test.close()
-pyscript.display(lines[0], target="output", append=False)
+# mip.install("http://localhost:8080/nltk.whl", "nltk")
+# # from nltk import *
+# # test = open("test.txt", "r")
+# # lines = test.readlines()
+# # test.close()
+# a = "<button id='my_button'>Click me!</button>"
+# self = type(document.body)
+# display(self, target="output", append=False)
+print(document)
 # import nltk
 # print(ntlk)
