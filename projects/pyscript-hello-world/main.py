d83a6d40c23bfa8b3734caefb4edb1d8d892645f
IsaacGaber <isaacjgaber@gmail.com>
2024-02-28 17:58:29 -0500

    changed behavior of main.py for test

--- projects/pyscript-hello-world/main.py
+++ projects/pyscript-hello-world/main.py
@@ -1,14 +1,17 @@
 from pyscript import document, display
-import mip
-
-# mip.install("http://localhost:8080/nltk.whl", "nltk")
-# # from nltk import *
-test = open("test.txt", "r")
-lines = test.readlines()
+# import mip
+#
+# # mip.install("http://localhost:8080/nltk.whl", "nltk")
+# # # from nltk import *
+# test = open("test.txt", "r")
+# lines = test.readlines()
 # # test.close()
 # a = "<button id='my_button'>Click me!</button>"
 # self = type(document.body)
-display(lines[0], target="output", append=False)
+i = 0
+while True:
+    display(i, target="output", append=False)
+    i += 1
 # print(document)
 # import nltk
 # print(ntlk)
