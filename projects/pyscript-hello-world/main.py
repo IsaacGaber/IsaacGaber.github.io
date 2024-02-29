9bb2bb5341bd639afb2edece481d1392d3a8f3d1
Isaac Gaber <159034893+IsaacGaber@users.noreply.github.com>
2024-02-29 01:04:20 -0500

    rewrote main.py as async

--- projects/pyscript-hello-world/main.py
+++ projects/pyscript-hello-world/main.py
@@ -8,10 +8,14 @@ from pyscript import document, display
 # # test.close()
 # a = "<button id='my_button'>Click me!</button>"
 # self = type(document.body)
-i = 0
-while True:
-    display("1", target="output", append=False)
-    i += 1
+import asyncio
+async def foo():
+    i = 0
+    while True:
+        await asyncio.sleep(.5)
+        Element("output").write(i)
+        i += 1
+pyscript.run_until_complete(foo())
 # print(document)
 # import nltk
 # print(ntlk)
