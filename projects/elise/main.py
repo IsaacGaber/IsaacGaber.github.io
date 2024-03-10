3cb3b0ca13fb4b790130aacc378d3e9a815a1922
IsaacGaber <isaacjgaber@gmail.com>
2024-03-09 23:50:14 -0500

    worked with Pyscript some More

--- projects/elise/main.py
+++ projects/elise/main.py
@@ -1,12 +1,18 @@
-from pyscript import document, display
 import asyncio
+from pyscript import document, display
+import js
+# import nltk
+# print(nltk)
 
-time = 0
 async def foo():
-  while True:
-      await asyncio.sleep(1)
-      time += 1
-      display(str(time), target="output")
+    time = 0
+    while True:
+        await asyncio.sleep(.01)
+        time += 1
+        display(str(time), target="output", append=False)
 # # await foo()
-
+# output = document.getElementById("output")
+# for key in output.dataset:
+#     print(key)
+# print(type(document.getElementById("output").dataset))
 asyncio.ensure_future(foo())
--- projects/pyscript-hello-world/main.py
+++ projects/pyscript-hello-world/main.py
@@ -1,24 +1,42 @@
-from pyscript import document, display
-# import mip
-#
-# # mip.install("http://localhost:8080/nltk.whl", "nltk")
-# # # from nltk import *
-# test = open("test.txt", "r")
-# lines = test.readlines()
-# # test.close()
-# a = "<button id='my_button'>Click me!</button>"
-# self = type(document.body)
-# mip.install("asyncio")
+# from pyscript import document, display
+# # import mip
+# #
+# # # mip.install("http://localhost:8080/nltk.whl", "nltk")
+# # # # from nltk import *
+# # test = open("test.txt", "r")
+# # lines = test.readlines()
+# # # test.close()
+# # a = "<button id='my_button'>Click me!</button>"
+# # self = type(document.body)
+# # mip.install("asyncio")
+# import asyncio
+# async def foo():
+#     i = 0
+#     while True:
+#         await asyncio.sleep(.5)
+#         # print(i)
+#         display(i, target="output", append=False))
+#         # Element("output").write(i)
+#         i += 1
+# pyscript.run_until_complete(foo())
+# # print(document)
+# # import nltk
+# # print(ntlk)
 import asyncio
-async def foo():
-    i = 0
-    while True:
-        await asyncio.sleep(.5)
-        # print(i)
-        display(i, target="output", append=False))
-        # Element("output").write(i)
-        i += 1
-pyscript.run_until_complete(foo())
-# print(document)
+from pyscript import document, display
+# import js
 # import nltk
-# print(ntlk)
+# print(nltk)
+
+async def foo():
+    time = 0
+    while True:
+        await asyncio.sleep(.01)
+        time += 1
+        display(str(time), target="output", append=False)
+# # await foo()
+# output = document.getElementById("output")
+# for key in output.dataset:
+#     print(key)
+# print(type(document.getElementById("output").dataset))
+asyncio.ensure_future(foo())
